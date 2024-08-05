2e6c5612daba7dd859fe29eb6bd155438fdeb65b
deemisterious <dimasixc22@gmail.com>
2024-08-05 22:11:39 +0700

    feat : add logout

--- routes/route.go
+++ routes/route.go
@@ -11,4 +11,5 @@ func SetUp(app *fiber.App) {
 	app.Post("/api/register", controllers.Register)
 	app.Post("/api/login", controllers.Login)
 	app.Get("/api/user", controllers.User)
+	app.Post("/api/logout", controllers.Logout)
 }
